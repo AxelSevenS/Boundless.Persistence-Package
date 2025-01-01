91fbff438c759ec136ed297e8ab80b71fff9f8e5
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-01-01 01:48:54 +0100

    refactor: Enhance data registration methods with logging options

--- SevenDev.Boundless.Persistence/src/Item/IItemData.cs
+++ SevenDev.Boundless.Persistence/src/Item/IItemData.cs
@@ -9,34 +9,36 @@ public interface IItemData : IUIObject {
 
 	public static IItemData? GetData(string key) => Registry.TryGetValue(key, out IItemData? data) ? data : null;
 	public static IItemData? GetData(IDataKeyProvider? keyProvider) => keyProvider is not null ? GetData(keyProvider.Key) : null;
-	public static bool RegisterData(IItemData data, bool overwrite = false) {
+	protected static bool RegisterData(IItemData data, bool log, bool overwrite = false) {
 		string key = data.KeyProvider.Key;
 		if (string.IsNullOrWhiteSpace(key)) {
-			GD.PrintErr("Could not register: Data key is null or empty.");
+			if (log) GD.PrintErr("Could not register: Data key is null or empty.");
 			return false;
 		}
 
-		if (Registry.ContainsKey(key) && !overwrite) {
-			GD.PrintErr($"Data with key {key} already exists.");
+		if (!overwrite && Registry.ContainsKey(key)) {
+			if (log) GD.PrintErr($"Data with key {key} already exists.");
 			return false;
 		}
 
 		Registry[key] = data;
-		GD.Print($"Registered {key} => {data}");
+		if (log) GD.Print($"Registered {key} => {data}");
 		return true;
 	}
+	public static bool RegisterData(IItemData data, bool overwrite = false) => RegisterData(data, true, overwrite);
 
-	public static bool UnregisterData(IItemData data) {
+	public static bool UnregisterData(IItemData data, bool log) {
 		string key = data.KeyProvider.Key;
 		if (string.IsNullOrWhiteSpace(key)) {
-			GD.PrintErr("Could not unregister: Data key is null or empty.");
+			if (log) GD.PrintErr("Could not unregister: Data key is null or empty.");
 			return false;
 		}
 
 		Registry.Remove(key);
-		GD.Print($"Unregistered {key} => {data}");
+		if (log) GD.Print($"Unregistered {key} => {data}");
 		return true;
 	}
+	public static bool UnregisterData(IItemData data) => UnregisterData(data, true);
 
 
 	public void Register(bool force = false) => RegisterData(this, force);
@@ -52,29 +54,32 @@ public interface IItemData<out T> : IItemData where T : IItem<T> {
 	public new IDataKeyProvider<T> KeyProvider { get; }
 
 
-	public static IItemData<T>? GetData(string key) => TypedRegistry.TryGetValue(key, out IItemData<T>? data) ? data : null;
-	public static IItemData<T>? GetData(IDataKeyProvider<T>? keyProvider) => keyProvider is not null ? GetData(keyProvider.Key) : null;
-	public static bool RegisterData(IItemData<T> data, bool overwrite = false) {
-		if (!IItemData.RegisterData(data, overwrite)) return false;
+	public static new IItemData<T>? GetData(string key) => TypedRegistry.TryGetValue(key, out IItemData<T>? data) ? data : null;
+	public static new IItemData<T>? GetData(IDataKeyProvider<T>? keyProvider) => keyProvider is not null ? GetData(keyProvider.Key) : null;
+	protected static bool RegisterData(IItemData<T> data, bool log, bool overwrite = false) {
+		if (!IItemData.RegisterData(data, false, overwrite)) return false;
 
 		if (TypedRegistry.ContainsKey(data.KeyProvider.Key) && !overwrite) {
-			GD.PrintErr($"Data with key {data.KeyProvider.Key} (type {typeof(T)}) already exists.");
+			if (log) GD.PrintErr($"Data with key {data.KeyProvider.Key} (type {typeof(T)}) already exists.");
 			return false;
 		}
 
 		string key = data.KeyProvider.Key;
 		TypedRegistry[key] = data;
-		GD.Print($"Registered {key} => {data} (type {typeof(T)})");
+		if (log) GD.Print($"Registered {key} => {data} (type {typeof(T)})");
 		return true;
 	}
-	public static bool UnregisterData(IItemData<T> data) {
-		if (!IItemData.UnregisterData(data)) return false;
+	public static bool RegisterData(IItemData<T> data, bool overwrite = false) => RegisterData(data, true, overwrite);
+
+	protected static bool UnregisterData(IItemData<T> data, bool log) {
+		if (!IItemData.UnregisterData(data, false)) return false;
 
 		string key = data.KeyProvider.Key;
 		TypedRegistry.Remove(key);
-		GD.Print($"Unregistered {key} => {data} (type {typeof(T)})");
+		if (log) GD.Print($"Unregistered {key} => {data} (type {typeof(T)})");
 		return true;
 	}
+	public static bool UnregisterData(IItemData<T> data) => RegisterData(data, true);
 
 	void IItemData.Register(bool force) => Register(force);
 	public new sealed void Register(bool force = false) => RegisterData(this, force);
